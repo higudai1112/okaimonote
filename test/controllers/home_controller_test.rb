819256ab62f9e6b597d766a6d42b973af34f335d
樋口大輔 <disk1@hdmba2025.local>
2025-09-18 18:05:49 +0900

    fix:テスト修正完了

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.create!(
+      email: "test@example.com",
+      password: "password",
+      nickname: "テストユーザー"
+    )
+    sign_in @user, scope: :user
+  end
+
   test "should get index" do
-    get home_home_url
+    get home_url
     assert_response :success
   end
 end
