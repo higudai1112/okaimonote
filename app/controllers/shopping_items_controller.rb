e7ebaee4e86bf121f5a86f9a80d21c8e91ee607e
樋口大輔 <disk1@hdmba2025.local>
2025-10-25 15:13:34 +0900

    add:買い物リスト作成
    【概要】
    リストの作成
    モーダル化
    商品の追加、編集、削除
    css修正

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -1,35 +1,54 @@
 class ShoppingItemsController < ApplicationController
   before_action :set_shopping_list
-  before_action :set_shopping_item
+  before_action :set_shopping_item, only: [ :edit, :update, :destroy]
 
   def create
+    @shopping_item = @shopping_list.shopping_items.new(shopping_item_params)
     if @shopping_item.save
-      redirect_to @shopping_list_path(@shopping_list)
+      redirect_to shopping_list_path(@shopping_list)
     else
-      redirect_ro @shopping_list_path(@shopping_list), alert: "商品を追加できませんでした"
+      redirect_to shopping_list_path(@shopping_list), alert: "商品を追加できませんでした"
+    end
+  end
+
+  def edit
+    respond_to do |format|
+      format.turbo_stream
+      format.html
     end
   end
 
   def update
-    if @shopping_item.update(shopping_item_params)
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to shopping_list_path(@shopping_list)}
+    respond_to do |format|
+      if params[:shopping_item] && params[:shopping_item].keys.any? { |k| %w[name memo].include?(k) }
+        #編集モーダル更新
+        if @shopping_item.update(shopping_item_params)
+          format.turbo_stream { render "update_from_edit" }
+          format.html { redirect_to shopping_list_path(@shopping_list) }
+        else
+          format.html {redirect_to shopping_list_path(@shopping_list), alert: "更新に失敗しました" }
+        end
+      else
+        #購入済みトグル切り替え
+        if @shopping_item.update(purchased: !@shopping_item.purchased)
+          format.turbo_stream
+          format.html { redirect_to shopping_list_path(@shopping_list)}
+        else
+          redirect_to shopping_list_path(@shopping_list), alert: "更新に失敗しました"
+        end
       end
-    else
-      redirect_to @shopping_list_path(@shopping_list), alert: "更新に失敗しました"
     end
   end
 
   def destroy
     @shopping_item.destroy
-    redirect_to @shopping_list_path(@shopping_list), status: :see_other
+    redirect_to shopping_list_path(@shopping_list), status: :see_other
   end
 
   private
 
   def set_shopping_list
-    @shopping_list = current_user.shopping_lists.find(params[:shopping_list_id])
+    @shopping_list = current_user.shopping_list
   end
 
   def set_shopping_item
@@ -37,6 +56,6 @@ class ShoppingItemsController < ApplicationController
   end
 
   def shopping_item_params
-    params.require(:shopping_item).permit(:name, :memo, :purchased)
+    params.require(:shopping_item).permit(:name, :memo)
   end
 end
--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -3,5 +3,4 @@ class ShoppingList < ApplicationRecord
   has_many :shopping_items, dependent: :destroy
 
   validates :name, presence: true
-  end
 end
