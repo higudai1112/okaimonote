72969d083fb0eb81765bac091f98e9863290f016
樋口大輔 <disk1@hdmba2025.local>
2025-10-31 17:49:46 +0900

    fix:pages_controllerを修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :guide ]
 
   def index; end
+
+  def guide; end
 end
