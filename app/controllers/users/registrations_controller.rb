9d5b526e9ab609982649e01a3ffe5097f06975a0
樋口大輔 <disk1@hdmba2025.local>
2025-10-31 23:40:43 +0900

    fix:エラー対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
-    super do |user|
-      flash.delete(:notice)
-      flash[:notice] = "ようこそ、#{user.nickname}さん！<br>さっそく始めましょう！"
-    end
+    super
   end
 end
